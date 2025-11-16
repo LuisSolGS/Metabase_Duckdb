46d595412c77d2713033af384e91246b01ea602c
LuisSolGS <167305924+LuisSolGS@users.noreply.github.com>
2025-11-16 17:31:23 -0300

    Fix with env variables

--- metabase-setup.sh
+++ metabase-setup.sh
@@ -2,13 +2,11 @@
 # Define the API endpoint
 echo 'Metabase is up. Running setup...';
 
-echo "$MB_USER_EMAIL";
-
 curl -X POST http://metabase:3000/api/setup -H 'Content-Type: application/json' \
--d '{
-  "token":"mysecrettoken123",
-  "prefs":{"site_name":"My Metabase"},"user":{"email":"admin@example.com","password":"StrongPass123!","first_name":"Admin","last_name":"User"}
-  }';
+-d "{
+  \"token\":\"$MB_TOKEN\",
+  \"prefs\":{\"site_name\":\"Metabase\"},\"user\":{\"email\":\"$MB_USER_EMAIL\",\"password\":\"$MB_USER_PASSWORD\",\"first_name\":\"$MB_FIRST_NAME\",\"last_name\":\"$MB_LAST_NAME\"}
+  }";
 
 echo 'Setup complete.';
 
@@ -17,51 +15,51 @@ echo 'Requesting session token...';
 SESSION_ID=$(curl http://metabase:3000/api/session/ \
   --request POST \
   --header 'Content-Type: application/json' \
-  --data '{
-  "password": "StrongPass123!",
-  "username": "admin@example.com"
-}')
+  --data "{
+  \"password\": \"$MB_USER_PASSWORD\",
+  \"username\": \"$MB_USER_EMAIL\"
+}")
 
 echo "Session token acquired: $SESSION_ID";
 
 echo 'Adding database connection to Metabase...';
 
-name=$(echo "$SESSION_ID" | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
-echo "$name"
+ID=$(echo "$SESSION_ID" | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
+echo "$ID"
 
 STATUS=$(curl http://metabase:3000/api/database/ \
   --request POST \
   --header 'Content-Type: application/json' \
-  --header "X-Metabase-Session: $name" \
-  --data '{
-    "engine": "postgres",
-      "name": "My New Database",
-      "details": {
-        "host": "db",
-        "port": 5432,
-        "dbname": "metabase",
-        "user": "metabase",
-        "password": "metabase",
-        "ssl": false
+  --header "X-Metabase-Session: $ID" \
+  --data "{
+    \"engine\": \"postgres\",
+    \"name\": \"My New Database\",
+    \"details\": {
+      \"host\": \"db\",
+      \"port\": 5432,
+      \"dbname\": \"$MB_DB_DBNAME\",
+      \"user\": \"$MB_DB_USER\",
+      \"password\": \"$MB_DB_PASS\",
+      \"ssl\": false
+    },
+    \"is_full_sync\": true,
+    \"schedules\": {
+      \"cache_field_values\": {
+        \"schedule_day\": \"sun\",
+        \"schedule_frame\": \"first\",
+        \"schedule_hour\": 0,
+        \"schedule_minute\": 0,
+        \"schedule_type\": \"hourly\"
       },
-      "is_full_sync": true,
-      "schedules": {
-        "cache_field_values": {
-        "schedule_day": "sun",
-        "schedule_frame": "first",
-        "schedule_hour": 0,
-        "schedule_minute": 0,
-        "schedule_type": "hourly"
-      },
-      "metadata_sync": {
-        "schedule_day": "sun",
-        "schedule_frame": "first",
-        "schedule_hour": 0,
-        "schedule_minute": 0,
-        "schedule_type": "hourly"
+      \"metadata_sync\": {
+        \"schedule_day\": \"sun\",
+        \"schedule_frame\": \"first\",
+        \"schedule_hour\": 0,
+        \"schedule_minute\": 0,
+        \"schedule_type\": \"hourly\"
       }
-      }
-}');
+    }
+  }");
 
 echo "$STATUS"
 
